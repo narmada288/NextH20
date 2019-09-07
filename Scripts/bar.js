f109fdae9e44b8b776fabe3fff463090f499d797
narmada288 <bnarmada.vrse@gmail.com>
2019-09-08 07:24:19 +1000

    looped 2 states data

--- Scripts/bar.js
+++ Scripts/bar.js
@@ -22,72 +22,84 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(d3.format(".2s"));
 
-var svg = d3.select("#d3Id").append("svg")
+var scodes = ['nsw', 'qld'];
+
+scodes.forEach(function(d){
+    loadGraph(d);
+});
+
+function loadGraph(d){
+
+    let scodeId = "#" + d + "Id";
+    let scodefile = "././" + d + "CropRainfall.csv";
+
+    var svg = d3.select(scodeId).append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("././NSWCropRainfall.csv", function(error, data) {
-  if (error) throw error;
+    d3.csv(scodefile, function(error, data) {
+    if (error) throw error;
 
-  var yearNames = d3.keys(data[0]).filter(function(key) { return key !== "CropDistrict"; });
+    var yearNames = d3.keys(data[0]).filter(function(key) { return key !== "CropDistrict"; });
 
-  data.forEach(function(d) {
-    d.falls = yearNames.map(function(name) { return {name: name, value: +d[name]}; });
-  });
+    data.forEach(function(d) {
+        d.falls = yearNames.map(function(name) { return {name: name, value: +d[name]}; });
+    });
 
-  x0.domain(data.map(function(d) { return d.CropDistrict; }));
-  x1.domain(yearNames).rangeRoundBands([0, x0.rangeBand()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(d.falls, function(d) { return d.value; }); })]);
+    x0.domain(data.map(function(d) { return d.CropDistrict; }));
+    x1.domain(yearNames).rangeRoundBands([0, x0.rangeBand()]);
+    y.domain([0, d3.max(data, function(d) { return d3.max(d.falls, function(d) { return d.value; }); })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("RainFall (mm)");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("RainFall (mm)");
 
-  var state = svg.selectAll(".state")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "g")
-      .attr("transform", function(d) { return "translate(" + x0(d.CropDistrict) + ",0)"; });
+    var state = svg.selectAll(".state")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "g")
+        .attr("transform", function(d) { return "translate(" + x0(d.CropDistrict) + ",0)"; });
 
-  state.selectAll("rect")
-      .data(function(d) { return d.falls; })
-    .enter().append("rect")
-      .attr("width", x1.rangeBand())
-      .attr("x", function(d) { return x1(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .style("fill", function(d) { return color(d.name); });
+    state.selectAll("rect")
+        .data(function(d) { return d.falls; })
+        .enter().append("rect")
+        .attr("width", x1.rangeBand())
+        .attr("x", function(d) { return x1(d.name); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
+        .style("fill", function(d) { return color(d.name); });
 
-  var legend = svg.selectAll(".legend")
-      .data(yearNames.slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    var legend = svg.selectAll(".legend")
+        .data(yearNames.slice().reverse())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
 
-});
+    });
+}
