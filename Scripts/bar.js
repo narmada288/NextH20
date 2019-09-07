6d8a3e797ff389fe39f2c1278a1f171f81d27321
narmada288 <bnarmada.vrse@gmail.com>
2019-09-08 06:37:07 +1000

    Update bar.js

--- Scripts/bar.js
+++ Scripts/bar.js
@@ -1,5 +1,5 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
+    width = 1140 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var x0 = d3.scale.ordinal()
