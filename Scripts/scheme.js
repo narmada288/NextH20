7d4bcc428d5a2da3a9aff12d07de0f137e584fd9
narmada288 <bnarmada.vrse@gmail.com>
2019-09-08 14:14:40 +1000

    Update scheme.js

--- Scripts/scheme.js
+++ Scripts/scheme.js
@@ -3,10 +3,8 @@ function(d){
     return { sName : d.SchemeName, pName : d.Provider }
 },
 d3.select("#schemeId")
-.data(data)
-.enter()
-.append("option")
-.attr("value", function(d){return d;})
-.text(function(d){
+    .append("option")
+    .attr("value", function(d){return d;})
+    .text(function(d){
     return d.pName;
                     }))
