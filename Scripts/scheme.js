cbc0312a371ce013118da87c68e78435275d65cf
narmada288 <bnarmada.vrse@gmail.com>
2019-09-08 14:08:42 +1000

    reading from csv file

--- Scripts/scheme.js
+++ Scripts/scheme.js
@@ -1,20 +1,12 @@
-var data = {
-    resource_id: '3d718d43-73f1-4c8e-b408-05f6c5429694'
-};
-
-$.ajax({
-    type: "POST",
-    url: 'https://www.data.qld.gov.au/api/3/action/datastore_search',
-    data: '{name: "abc" }',
-    contentType: "application/json; charset=utf-8",
-    dataType: "jsonp",
-    success: function(d)
-            {
-                $.each(data.d, function (){
-                    $(".schemeId").append($("<option     />").val(this.KeyName).text(this.ValueName));
-                });
-            },
-    failure: function () {
-        alert("Failed!");
-    }
-});
+d3.csv("././recycledSchemes.csv",
+function(d){
+    return { sName : d.SchemeName, pName : d.Provider }
+},
+d3.select("#schemeId")
+.data(data)
+.enter()
+.append("option")
+.attr("value", function(d){return d;})
+.text(function(d){
+    return d.pName;
+                    }))
